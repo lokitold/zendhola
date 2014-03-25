d2c2511861b4336715949fc2863d83c8c99f4302
vrojas <vrojas@ec.pe>
2014-03-25 10:11:32 -0500

    cambios

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -7,11 +7,35 @@ class IndexController extends Zend_Controller_Action
     {
         /* Initialize action controller here */
     }
-
-    public function indexAction()
+    public function blank_pageAction()
     {
         
     }
+    public function bootstrap_elementsAction()
+    {
+        
+    } 
+    public function bootstrap_gridAction()
+    {
+        
+    }
+    public function chartsAction()
+    {
+        
+    }
+    public function formsAction()
+    {
+        
+    }
+     public function tablesAction()
+    {
+        
+    }
+     public function typographyAction()
+    {
+        
+    }
+
 
 
 }
