accbdb0dbb118ca1be2dbb70c034761a30fb856c
lokito <lokito@lokito-desktop.(none)>
2014-03-31 06:26:31 -0500

    cambios31

---
+++ application/controllers/PostsController.php
@@ -0,0 +1,11 @@
+<?php
+class PostsController extends Zend_Controller_Action {
+    public function listarAction(){
+        $model = new Application_Model_Posts();
+        $this->view->posts = $model->getAll();
+    }
+    public function agregarAction(){
+        
+    }
+}
+
